d388ea50ada2eeb6def81f3caaa47bf511ceda9a
Daniel Schmidt <dschmidt@weluse.de>
2015-04-08 16:06:32 -0400

    reinit as yo backbone

--- frontend/app/scripts/main.js
+++ frontend/app/scripts/main.js
@@ -1,10 +1,23 @@
+/*global require*/
 'use strict';
 
-import {DanceTrainingCompanion} from 'views/danceTrainingCompanion';
+require.config({
+    shim: {
+        bootstrap: {
+            deps: ['jquery'],
+            exports: 'jquery'
+        }
+    },
+    paths: {
+        jquery: '../bower_components/jquery/dist/jquery',
+        backbone: '../bower_components/backbone/backbone',
+        underscore: '../bower_components/lodash/dist/lodash',
+        bootstrap: '../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap'
+    }
+});
 
-debugger
-
-React.render(
-  <DanceTrainingCompanion />,
-  document.querySelector('#content')
-);
+require([
+    'backbone'
+], function (Backbone) {
+    Backbone.history.start();
+});
