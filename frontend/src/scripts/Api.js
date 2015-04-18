2d70c1070de99b9e858673627cae1cfbcb738865
Daniel Schmidt <dschmidt@weluse.de>
2015-04-18 12:51:55 -0400

    added todos

--- frontend/src/scripts/Api.js
+++ frontend/src/scripts/Api.js
@@ -3,22 +3,26 @@
 var API_ENDPOINT = '';
 
 var Api = {
+  // TODO: get rid of token and hold user information required to login here
   loadNotesFor: function(token, promise) {
     if (promise === undefined) {
       promise = new Promise();
     }
     // TODO: make a real api request
+    // TODO: cache via localstorage and then background sync
 
     promise.resolve(function() {
       return {};
     });
   },
 
+  // TODO: get rid of token and hold user information required to login here
   loadChoreosFor: function(token, promise) {
     if (promise === undefined) {
       promise = new Promise();
     }
     // TODO: make a real api request
+    // TODO: cache via localstorage and then background sync
 
     promise.resolve([{
       name: 'Samba',
